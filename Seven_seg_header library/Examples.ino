260f89d8b11067153916a28490700c44fe341ec9
Rechard Nabo <32723733+RechardNabo@users.noreply.github.com>
2020-07-22 15:12:57 +0200

    Update Examples.ino

--- Seven_seg_header library/Examples.ino
+++ Seven_seg_header library/Examples.ino
@@ -22,13 +22,18 @@
 // Examples bellow
 
 #include "Seeven_seg_lib_nonmultiplex.h"
-
+#define ground1 2
+#define ground2 3
 void setup() {
+  pinMode(ground1,OUTPUT);
+  pinMode(ground2,OUTPUT);
 }
 
 //==============================================[up counter function]=============================================
 void Upcounter(){
   Segment Segment(35,37,43,41,39,33,31,45,'A');
+  digitalWrite(ground1,LOW);
+  digitalWrite(ground2,LOW);
   int counter = 0x00;
   while(true){
     if(counter ==0x0A){
@@ -43,6 +48,8 @@ void Upcounter(){
 //==============================================[down counter function]=============================================
 void Downcounter(){
   Segment Segment(35,37,43,41,39,33,31,45,'A');
+  digitalWrite(ground1,LOW);
+  digitalWrite(ground2,LOW);
   int counter = 0x09;
   while(true){
     if(counter ==0x00){
@@ -56,11 +63,13 @@ void Downcounter(){
 //==============================================[display number function]=============================================
 void myNumber(){
     Segment Segment(35,37,43,41,39,33,31,45,'A');
+    digitalWrite(ground1,LOW);
+    digitalWrite(ground2,LOW);
     Segment.Display(0x08); // display 8 in hex 0x08  
   }
 
 void loop() {
-  //Upcounter();
+  Upcounter();
   //Downcounter();
   //myNumber();
 }
